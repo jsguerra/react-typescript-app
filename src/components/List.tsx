5798a046d9ca4f4e04714f98a73ddbb9a9c3e52d
jsguerra <jose@gcostudios.com>
2021-10-24 21:05:38 +0100

    initial commit

---
+++ src/components/List.tsx
@@ -0,0 +1,37 @@
+import { prependOnceListener } from "process";
+import React from "react";
+
+interface IProps {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
+const List: React.FC<IProps> = ({ people }) => {
+  const renderList = (): JSX.Element[] => {
+    return people.map((person) => {
+      return (
+        <li className="List">
+          <div className="List-eader">
+            <img className="List-img" src={person.url} />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+
+  return <ul>{renderList()}</ul>;
+};
+
+// Instead of passing props we can define it as a function component (FC) as above
+// const List = (props: IProps) => {
+//   return <div>I am a list</div>;
+// };
+
+export default List;
